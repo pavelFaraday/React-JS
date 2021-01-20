57257f35659778d22aca66db843dfa0a546ecaf5
pavelFaraday <devstudio.corp@gmail.com>
2021-01-20 23:28:42 +0400

    React Lessons | props

--- react-lessons/src/App.js
+++ react-lessons/src/App.js
@@ -2,11 +2,14 @@
 import React from 'react';
 import Hello from './sayHello';
 
+
 function myFun() {
   return (
     <div className='height_App'>
-      <h1>This is myFun's Component.</h1>
-      <Hello/>  
+      <Hello name='Dev Ed' message='Good morning' numberLikes='128K' btnValue='Dev Says:' />  
+      <Hello name='Mosh' message='Good Night' numberLikes='232K' btnValue='Mosh Says:' />  
+      <Hello name='Winter is Coming' message='Happy Birthday' numberLikes='70K' btnValue='Winter Says:' />  
+      <Hello name='Gulfo' message='Happy new Year' numberLikes='12K' btnValue='Gulfo Says:' />  
     </div>
   );
 }
--- react-lessons/src/sayHello.js
+++ react-lessons/src/sayHello.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import './App.css';
 
-function Hello() {
-    const sayHello = () => {
-      alert('Hello');
-    };
+function Hello({name, message, numberLikes, btnValue}) {
+  const sayHello = () => {
+    alert(`Hey you, ${message}`);
+  };
+
+  return (
+    <div className='bordRed'>
+      <h3>{name}</h3>
+      <p>{message}</p>
+      <p>LIKES: {numberLikes}</p>
+      <button onClick={sayHello}>{btnValue}</button>
+    </div>
+  );
+} 
+
+export default Hello;
+
+/*  ------- OR -------
+function Hello(props) {
+  const sayHello = () => {
+    alert('Hello');
+  };
+
+  return (
+    <div className='bordRed'>
+      <h3>{props.name}</h3>
+      <p>{props.message}</p>
+      <p>LIKES: {props.numberLikes}</p>
+      <button onClick={sayHello}>Hello</button>
+    </div>
+  );
+}
+*/
   
-    return (
-      <div className='bordRed'>
-        <h3>This is sayHello's Component.</h3>
-        <button onClick={sayHello}>Hello</button>
-      </div>
-    );
-  }
-  
-  export default Hello;
