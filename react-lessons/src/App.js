9117a7c6aeab985960c9f172566cb5929d96b4ca
pavelFaraday <devstudio.corp@gmail.com>
2021-01-20 19:37:33 +0400

    add javaScript  Logic to html

--- react-lessons/src/App.js
+++ react-lessons/src/App.js
@@ -1,13 +1,26 @@
+// 2) add javaScript  Logic tO html
+// Everithing in {} is javascript Code
+// We put {} into component's html code, for adding JS logic to Html
+
 import React from 'react';
 
-// 1) Create first Component
 function myFun() {
+  
+  const sayHello = () => {
+    alert('Hello');
+  };
+
+  let counter = 156686;
+  let myPara = 'It is variable in Paragraph with React.';
+
   return (
     <div>
       <h1>Hello World</h1>
+      <p>{myPara}</p>
+      <button onClick={sayHello}>Hello</button> 
+      <input type='text' value={counter}></input> 
     </div>
   );
 }
 
-// 2) Export Component
 export default myFun;
