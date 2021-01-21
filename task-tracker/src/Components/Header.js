60041e514b49982f4a539c347548217ca1eac858
pavelFaraday <devstudio.corp@gmail.com>
2021-01-21 22:26:44 +0400

    React Task Trecker | Styling Components

--- task-tracker/src/Components/Header.js
+++ task-tracker/src/Components/Header.js
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types'
 const Header = ({title}) => {
     return (
         <header>
-            <h1>{title}</h1>
+            {/* inline Styles */}
+            <h1 style={{color: 'red', border: '1px solid blue', background: 'black' }}>{title}</h1>
+
+            {/* Internal Styles */}
+            <p style={ParaStyle}>I am Styled too</p>
         </header>
     )
 }
@@ -17,6 +21,12 @@ Header.defaultProps = {
 // propTypes
 Header.propTypes = {
     title: PropTypes.string,
+}
+
+// Internal Styles
+const ParaStyle = {
+    color: 'blue',
+    fontWeight: 'bold'
 }
 
 
