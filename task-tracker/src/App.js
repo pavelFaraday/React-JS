fba6cd3f465ba82f037e9a673f58a9fb568eb76b
pavelFaraday <devstudio.corp@gmail.com>
2021-01-22 09:40:56 +0400

    React Task Tracker | states | map in states

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,9 +1,35 @@
+import { useState } from 'react'
 import Header from './Components/Header'
+import Tasks from './Components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        { 
+            id: 1,
+            text: 'Meeting at Work',
+            day: '24 February',
+            reminder: true
+        },
+        { 
+            id: 2,
+            text: 'Eat Glazures',
+            day: '31 March',
+            reminder: true
+        },
+        { 
+            id: 3,
+            text: 'Run',
+            day: '7 Jule',
+            reminder: false
+        }
+    ]
+)
+
   return (
     <div className="container">
-      <Header title={123} />
+      <Header title='Task Tracker' />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
---
+++ task-tracker/src/Components/Tasks.js
@@ -0,0 +1,15 @@
+
+
+const Tasks = ({tasks}) => {
+
+    return (
+        <>
+            {tasks.map((task) => (
+                    <h3 key={task.id}>{task.text}</h3>
+                )
+            )}
+        </>
+    )
+}
+
+export default Tasks
