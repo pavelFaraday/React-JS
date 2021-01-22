a45bf8ebefbe54af25533aad4519229e023cd203
pavelFaraday <devstudio.corp@gmail.com>
2021-01-22 14:19:33 +0400

    React Task Tracker | Toggle Reminder

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -31,10 +31,17 @@ const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => 
+  task.id === id ? { ...task, reminder: !task.reminder } : task))
+}
+
   return (
     <div className="container">
       <Header title='Task Tracker' />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No tasks to show!'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> 
+      : 'No tasks to show!'}
     </div>
   );
 }
