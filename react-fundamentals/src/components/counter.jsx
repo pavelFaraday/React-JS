56025587768f3b79f186fdc0a1905196e4aeb481
pavelFaraday <devstudio.corp@gmail.com>
2021-01-24 21:55:54 +0400

    React Fundamentals | embedding expressions

--- react-fundamentals/src/components/counter.jsx
+++ react-fundamentals/src/components/counter.jsx
@@ -3,13 +3,24 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
+    state = {
+        count: 0
+    }
+
     render() { 
         return (
         <React.Fragment>
-            <h1>Hello John</h1>
+            <span>{this.formatCount()}</span>
+            <button>Increment</button>
         </React.Fragment>
         )
     }
+
+    formatCount () {
+        const { count } = this.state;
+        return count === 0 ? <h1>Zero</h1> : count;
+    }
+
 }
  
 export default Counter;
