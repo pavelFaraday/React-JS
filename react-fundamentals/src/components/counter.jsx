9cf989cd036788b82f3cb8b0ec3ff9d204357e7f
pavelFaraday <devstudio.corp@gmail.com>
2021-01-24 21:35:41 +0400

    React Fundamentals | <React.Fragment>

--- react-fundamentals/src/components/counter.jsx
+++ react-fundamentals/src/components/counter.jsx
@@ -1,8 +1,14 @@
+// </React.Fragment> --- When we want not to display parent div
+
 import React, { Component } from 'react'
 
 class Counter extends Component {
     render() { 
-        return <h1>Hello John</h1>
+        return (
+        <React.Fragment>
+            <h1>Hello John</h1>
+        </React.Fragment>
+        )
     }
 }
  
