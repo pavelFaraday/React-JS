a01a2b8da72fdb11e8873e8110417197fc112831
pavelFaraday <devstudio.corp@gmail.com>
2021-01-25 19:22:12 +0400

    React Fundamentals | Composing Components

--- react-fundamentals/src/components/counter.jsx
+++ react-fundamentals/src/components/counter.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
+
     state = {
         count: 0,
     }
@@ -10,24 +11,25 @@ class Counter extends Component {
         fontWeight: 'bold'
     };
 
-    // you must bind every Event to 'this' keyword with constructor function
-  /*   constructor() {
-        super();
-        this.handleIncrement = this.handleIncrement.bind(this); 
-    } */
-
-    // or remake event into an arrow function
     handleIncrement = () => {
-        console.log('I handle', this);
+        // setstate
+        this.setState({ count: this.state.count + 1 });
     }
 
     render() { 
         return (
         <React.Fragment>
-            <span style={this.styles} className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+            <br/>
         </React.Fragment>
         )
+    }
+
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
     }
 
     formatCount () {
