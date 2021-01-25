b20f61d6c9512014da71c274fa72e05338f6ce33
pavelFaraday <devstudio.corp@gmail.com>
2021-01-25 10:49:42 +0400

    React Fundamentals | Rennderring Lists Dynamically

--- react-fundamentals/src/components/counter.jsx
+++ react-fundamentals/src/components/counter.jsx
@@ -4,7 +4,8 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
     }
 
     render() { 
@@ -12,6 +13,9 @@ class Counter extends Component {
         <React.Fragment>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
+            <ul>
+                {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+            </ul>
         </React.Fragment>
         )
     }
