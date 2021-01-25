2944ebab894c5a05e1724944e000ab17ab8e25ed
pavelFaraday <devstudio.corp@gmail.com>
2021-01-25 10:25:45 +0400

    React Fundamentals | add style 2

--- react-fundamentals/src/components/counter.jsx
+++ react-fundamentals/src/components/counter.jsx
@@ -7,15 +7,10 @@ class Counter extends Component {
         count: 0,
     }
 
-    styles = {
-        fontSize: 14,
-        fontWeight: 'bold'
-    };
-
     render() { 
         return (
         <React.Fragment>
-            <span style={this.styles} className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <span style={{fontWeight: 'bold', fontSize:14}} className='badge badge-primary m-2'>{this.formatCount()}</span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
         </React.Fragment>
         )
