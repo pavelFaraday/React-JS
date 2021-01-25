059ccee9192610d4ad709976c347ec803bf5f3de
pavelFaraday <devstudio.corp@gmail.com>
2021-01-25 10:38:18 +0400

    React Fundamentals | add classes dynamically | Refactoring

--- react-fundamentals/src/components/counter.jsx
+++ react-fundamentals/src/components/counter.jsx
@@ -4,16 +4,22 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count: 0,
+        count: 0
     }
 
     render() { 
         return (
         <React.Fragment>
-            <span style={{fontWeight: 'bold', fontSize:14}} className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
         </React.Fragment>
         )
+    }
+
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
     }
 
     formatCount () {
